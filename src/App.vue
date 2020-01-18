b1c63905cfad1a34540a081ffffd218576e62efa
Brock Wilcox <awwaiid@thelackthereof.org>
2020-01-18 17:56:33 -0500

    Basic beeping and hex grid

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,168 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="odd-row">
+      <TriggerButton note="70" :audio-context="audioContext" />
+      <TriggerButton note="72" :audio-context="audioContext" />
+      <TriggerButton note="74" :audio-context="audioContext" />
+
+      <TriggerButton note="76" :audio-context="audioContext" />
+      <TriggerButton note="78" :audio-context="audioContext" />
+      <TriggerButton note="80" :audio-context="audioContext" />
+
+      <TriggerButton note="82" :audio-context="audioContext" />
+      <TriggerButton note="84" :audio-context="audioContext" />
+      <TriggerButton note="86" :audio-context="audioContext" />
+    </div>
+    <div class="even-row">
+      <TriggerButton note="63" :audio-context="audioContext" />
+      <TriggerButton note="65" :audio-context="audioContext" />
+      <TriggerButton note="67" :audio-context="audioContext" />
+
+      <TriggerButton note="69" :audio-context="audioContext" />
+      <TriggerButton note="71" :audio-context="audioContext" />
+      <TriggerButton note="73" :audio-context="audioContext" />
+      <TriggerButton note="75" :audio-context="audioContext" />
+
+      <TriggerButton note="77" :audio-context="audioContext" />
+      <TriggerButton note="79" :audio-context="audioContext" />
+      <TriggerButton note="81" :audio-context="audioContext" />
+    </div>
+    <div class="odd-row">
+      <TriggerButton note="58" :audio-context="audioContext" />
+      <TriggerButton note="60" :audio-context="audioContext" />
+      <TriggerButton note="62" :audio-context="audioContext" />
+
+      <TriggerButton note="64" :audio-context="audioContext" />
+      <TriggerButton note="66" :audio-context="audioContext" />
+      <TriggerButton note="68" :audio-context="audioContext" />
+
+      <TriggerButton note="70" :audio-context="audioContext" />
+      <TriggerButton note="72" :audio-context="audioContext" />
+      <TriggerButton note="74" :audio-context="audioContext" />
+    </div>
+    <div class="even-row">
+      <TriggerButton note="51" :audio-context="audioContext" />
+      <TriggerButton note="53" :audio-context="audioContext" />
+      <TriggerButton note="55" :audio-context="audioContext" />
+
+      <TriggerButton note="57" :audio-context="audioContext" />
+      <TriggerButton note="59" :audio-context="audioContext" />
+      <TriggerButton note="61" :audio-context="audioContext" />
+      <TriggerButton note="63" :audio-context="audioContext" />
+
+      <TriggerButton note="65" :audio-context="audioContext" />
+      <TriggerButton note="67" :audio-context="audioContext" />
+      <TriggerButton note="69" :audio-context="audioContext" />
+    </div>
+    <div class="odd-row">
+      <TriggerButton note="46" :audio-context="audioContext" />
+      <TriggerButton note="48" :audio-context="audioContext" />
+      <TriggerButton note="50" :audio-context="audioContext" />
+
+      <TriggerButton note="52" :audio-context="audioContext" />
+      <TriggerButton note="54" :audio-context="audioContext" />
+      <TriggerButton note="56" :audio-context="audioContext" />
+
+      <TriggerButton note="58" :audio-context="audioContext" />
+      <TriggerButton note="60" :audio-context="audioContext" />
+      <TriggerButton note="62" :audio-context="audioContext" />
+    </div>
+    <div class="even-row">
+      <TriggerButton note="39" :audio-context="audioContext" />
+      <TriggerButton note="41" :audio-context="audioContext" />
+      <TriggerButton note="43" :audio-context="audioContext" />
+
+      <TriggerButton note="45" :audio-context="audioContext" />
+      <TriggerButton note="47" :audio-context="audioContext" />
+      <TriggerButton note="49" :audio-context="audioContext" />
+      <TriggerButton note="51" :audio-context="audioContext" />
+
+      <TriggerButton note="53" :audio-context="audioContext" />
+      <TriggerButton note="55" :audio-context="audioContext" />
+      <TriggerButton note="57" :audio-context="audioContext" />
+    </div>
+    <div class="odd-row">
+      <TriggerButton note="34" :audio-context="audioContext" />
+      <TriggerButton note="36" :audio-context="audioContext" />
+      <TriggerButton note="38" :audio-context="audioContext" />
+
+      <TriggerButton note="40" :audio-context="audioContext" />
+      <TriggerButton note="42" :audio-context="audioContext" />
+      <TriggerButton note="44" :audio-context="audioContext" />
+
+      <TriggerButton note="46" :audio-context="audioContext" />
+      <TriggerButton note="48" :audio-context="audioContext" />
+      <TriggerButton note="50" :audio-context="audioContext" />
+    </div>
+    <div class="even-row">
+      <TriggerButton note="27" :audio-context="audioContext" />
+      <TriggerButton note="29" :audio-context="audioContext" />
+      <TriggerButton note="31" :audio-context="audioContext" />
+
+      <TriggerButton note="33" :audio-context="audioContext" />
+      <TriggerButton note="35" :audio-context="audioContext" />
+      <TriggerButton note="37" :audio-context="audioContext" />
+      <TriggerButton note="39" :audio-context="audioContext" />
+
+      <TriggerButton note="41" :audio-context="audioContext" />
+      <TriggerButton note="43" :audio-context="audioContext" />
+      <TriggerButton note="45" :audio-context="audioContext" />
+    </div>
+    <div class="odd-row">
+      <TriggerButton note="22" :audio-context="audioContext" />
+      <TriggerButton note="24" :audio-context="audioContext" />
+      <TriggerButton note="26" :audio-context="audioContext" />
+
+      <TriggerButton note="28" :audio-context="audioContext" />
+      <TriggerButton note="30" :audio-context="audioContext" />
+      <TriggerButton note="32" :audio-context="audioContext" />
+
+      <TriggerButton note="34" :audio-context="audioContext" />
+      <TriggerButton note="36" :audio-context="audioContext" />
+      <TriggerButton note="38" :audio-context="audioContext" />
+    </div>
+    <div class="even-row">
+      <TriggerButton note="15" :audio-context="audioContext" />
+      <TriggerButton note="17" :audio-context="audioContext" />
+      <TriggerButton note="19" :audio-context="audioContext" />
+
+      <TriggerButton note="21" :audio-context="audioContext" />
+      <TriggerButton note="23" :audio-context="audioContext" />
+      <TriggerButton note="25" :audio-context="audioContext" />
+      <TriggerButton note="27" :audio-context="audioContext" />
+
+      <TriggerButton note="29" :audio-context="audioContext" />
+      <TriggerButton note="31" :audio-context="audioContext" />
+      <TriggerButton note="33" :audio-context="audioContext" />
+    </div>
+    <div class="odd-row">
+      <TriggerButton note="10" :audio-context="audioContext" />
+      <TriggerButton note="12" :audio-context="audioContext" />
+      <TriggerButton note="14" :audio-context="audioContext" />
+
+      <TriggerButton note="16" :audio-context="audioContext" />
+      <TriggerButton note="18" :audio-context="audioContext" />
+      <TriggerButton note="20" :audio-context="audioContext" />
+
+      <TriggerButton note="22" :audio-context="audioContext" />
+      <TriggerButton note="24" :audio-context="audioContext" />
+      <TriggerButton note="26" :audio-context="audioContext" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TriggerButton from "./components/TriggerButton.vue";
 
 export default {
   name: "app",
+  data() {
+    return {
+      audioContext: new AudioContext()
+    };
+  },
   components: {
-    HelloWorld
+    TriggerButton
   }
 };
 </script>
@@ -24,5 +175,29 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  .odd-row,
+  .even-row {
+    clear: both;
+
+    .trigger-button {
+      float: left;
+      margin-top: -15px;
+      margin-bottom: -15px;
+      margin-left: 0px;
+    }
+  }
+
+  .odd-row {
+    margin-left: 25px;
+    .trigger-button {
+    }
+  }
+
+  .hexagon2 {
+    width: 50px;
+    height: 100px;
+    margin: -20px 0 0 5px;
+  }
 }
 </style>
---
+++ src/components/TriggerButton.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="trigger-button">
+    <div class="hexagon hexagon2">
+      <div class="hexagon-in1">
+        <div class="hexagon-in2" @mousedown="beep">
+          {{ this.$props.note }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TriggerButton",
+  props: {
+    note: String,
+    audioContext: AudioContext
+  },
+  // data() {
+  //   return {
+  //     audioContext: new AudioContext()
+  //   };
+  // },
+  // created() {
+  //   let a = new AudioContext(); // browsers limit the number of concurrent audio contexts, so you better re-use'em
+  //   this.$data.audoContext = a;
+  // },
+  methods: {
+    noteToFreq(note) {
+      return 2 ** ((note - 49) / 12) * 440;
+    },
+    beepity(vol, freq, duration) {
+      console.log({ vol, freq, duration });
+      let a = this.$props.audioContext;
+      console.log({ currentTime: a.currentTime });
+      let v = a.createOscillator();
+      let u = a.createGain();
+      v.connect(u);
+      v.frequency.value = freq;
+      v.type = "square";
+      u.connect(a.destination);
+      u.gain.value = vol * 0.01;
+      v.start(a.currentTime);
+      v.stop(a.currentTime + duration * 0.001);
+    },
+    beep() {
+      console.log("x");
+      this.beepity(80, this.noteToFreq(this.$props.note), 80);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hexagon {
+  overflow: hidden;
+  visibility: hidden;
+  -webkit-transform: rotate(120deg);
+  -moz-transform: rotate(120deg);
+  -ms-transform: rotate(120deg);
+  -o-transform: rotate(120deg);
+  transform: rotate(120deg);
+  cursor: pointer;
+}
+
+.hexagon-in1 {
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  -webkit-transform: rotate(-60deg);
+  -moz-transform: rotate(-60deg);
+  -ms-transform: rotate(-60deg);
+  -o-transform: rotate(-60deg);
+  transform: rotate(-60deg);
+}
+
+.hexagon-in2 {
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-position: 50%;
+  /* background-image: url(http://placekitten.com/240/240); */
+  background-color: red;
+  visibility: visible;
+  -webkit-transform: rotate(-60deg);
+  -moz-transform: rotate(-60deg);
+  -ms-transform: rotate(-60deg);
+  -o-transform: rotate(-60deg);
+  transform: rotate(-60deg);
+}
+
+.hexagon-in2:hover {
+  /* background-image: url(http://placekitten.com/241/241); */
+  background-color: green;
+}
+</style>
