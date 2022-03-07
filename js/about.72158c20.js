6c7e2d9fb8b88f7a8ba736c98b8784ce134230ff
Brock Wilcox <awwaiid@thelackthereof.org>
2022-03-06 22:38:57 -0500

    deploy

---
+++ js/about.72158c20.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")]),n("p",[t._v("Yep. It's pretty darn epic.")])])}],r=n("2877"),u={},c=Object(r["a"])(u,a,s,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=about.72158c20.js.map
---
+++ precache-manifest.7b60904a2488d0625e551e4d4d6c7d5c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2907ae68775180043bfd",
+    "url": "/css/app.2bc41efa.css"
+  },
+  {
+    "revision": "97cccb629490d267f928",
+    "url": "/css/leaders.08f51eff.css"
+  },
+  {
+    "revision": "0b6064d872f40c128dd2",
+    "url": "/css/synth.477c03c7.css"
+  },
+  {
+    "revision": "8ead7831d2eb7cdc496fd7b833728a48",
+    "url": "/img/mizik-logo-02.8ead7831.png"
+  },
+  {
+    "revision": "5d2cc39bcfc92f953e485032adc98d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c4d73103f0ac24b22c",
+    "url": "/js/about.72158c20.js"
+  },
+  {
+    "revision": "2907ae68775180043bfd",
+    "url": "/js/app.42764b8c.js"
+  },
+  {
+    "revision": "e5acc1545934cb2d5768",
+    "url": "/js/chunk-vendors.35cde30e.js"
+  },
+  {
+    "revision": "97cccb629490d267f928",
+    "url": "/js/leaders.fbb20e05.js"
+  },
+  {
+    "revision": "5749265391292dd9dbe4",
+    "url": "/js/leaders~synth.6bb5f8b3.js"
+  },
+  {
+    "revision": "0b6064d872f40c128dd2",
+    "url": "/js/synth.d84ddf1d.js"
+  },
+  {
+    "revision": "4bf5de1791dd5ff996d24f61d553389d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b86904ef411bc5d1a29f457ede36bf9e",
+    "url": "/mizik-20200202.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "aac0a526bd9f691dcaa8d6bd2b22ba67",
+    "url": "/synth-20200119.png"
+  }
+]);
